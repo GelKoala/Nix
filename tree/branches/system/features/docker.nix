e68aaa95e0575fed586f8b6bb43f8275cbab8a6b
GelKoala <GelKola@proton.me>
2026-04-30 05:53:25 -0300

    nix

--- tree/branches/system/features/docker.nix
+++ tree/branches/system/features/docker.nix
@@ -8,7 +8,8 @@
     users.users.${UserConfig.username}.extraGroups = [ "docker" ];
 
     environment.systemPackages = with pkgs; [
-      docker-desktop
+      docker
+      docker-compose
     ];
   };
 }
