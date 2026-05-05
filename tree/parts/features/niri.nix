0238c53ebe4a03d6eeb66fd371af05528c990b6b
GelKoala <GelKola@proton.me>
2026-05-05 19:41:15 -0300

    main

--- tree/parts/features/niri.nix
+++ tree/parts/features/niri.nix
@@ -19,7 +19,12 @@
         layout.gaps = 2;
         binds = {
           "Mod+T".spawn-sh = lib.getExe pkgs.kitty;
-          "Mod+Q".close-window = _: { };
+          "Mod+Q".close-window = _: {};
+          "Mod+Left".focus-column-left = _: {};
+          "Mod+Right".focus-column-right = _: {};
+          "Mod+Shift+Left".move-column-left = _: {};
+          "Mod+Shift+Right".move-column-right = _: {};
+          "Mod+D".spawn-sh = lib.getExe pkgs.vesktop;
         };
       };
     };
