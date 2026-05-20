92b6a2da7bb65030e4b3bfe5b6734ed6eb9308eb
GelKoala <GelKola@proton.me>
2026-05-20 17:42:36 -0300

    main

--- tree/parts/overlays.nix
+++ tree/parts/overlays.nix
@@ -1,7 +1,7 @@
 { inputs, SystemConfig, pkgs,  ... }:
 {
   flake.overlays.default = final: prev: {
-    zen-browser = inputs.zen-browser.packages.${prev.system}.default;
+    zen-browser = inputs.zen-browser.packages.${prev.stdenv.hostPlatform.system}.default;
     stable = import inputs.nixpkgs-stable {
       system = prev.system;
       config.allowUnfree = true;
