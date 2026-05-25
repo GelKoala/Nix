d75cc945fabadc2adeb6e88010bf4ddcfafeedc0
GelKoala <GelKola@proton.me>
2026-05-24 21:43:10 -0300

    main

--- tree/parts/modules.nix
+++ tree/parts/modules.nix
@@ -1,7 +1,15 @@
-{ inputs, ... }: {
+{ inputs, self, ... }: {
   flake = {
     modules.system = inputs.import-tree ../modules/system;
     modules.home = inputs.import-tree ../modules/home;
     modules.shell = inputs.import-tree ../modules/shell;
+  };
+
+  perSystem = { system, ... }: {
+    _module.args.pkgs = import inputs.nixpkgs {
+      inherit system;
+      config.allowUnfree = true;
+      overlays = [ self.overlays.default ];
+    };
   };
 }
